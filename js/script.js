3fa81460a47b950b2c73f713bda348b66e179276
mmdkh <mohamaddadkhah69@gmail.com>
2022-08-12 14:44:48 +0430

    scroll

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
-
-
 const hireMe = document.getElementById('hireMe');
-const contactSec = document.getElementById('contact')
+const contactSec = document.getElementById('Contact');
+const links = document.querySelectorAll('.nav_bar__link')
 
-hireMe.addEventListener('click' , function (e) {
-    e.preventDefault();
-    contactSec.scrollIntoView({behavior : 'smooth'})
+const scorollTo = function (btn , section) {
+    btn.addEventListener('click' , function (e) {
+        e.preventDefault();
+        section.scrollIntoView({behavior : 'smooth'})
+    })
+}
+
+//////////////////
+scorollTo(hireMe , contactSec)
+
+
+links.forEach(link => {
+    const section = document.getElementById(link.textContent)
+    link.addEventListener('click' , scorollTo(link , section))
 })
